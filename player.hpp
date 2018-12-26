427040dc9304d8b332d91ad69cc27ba4692f9753
Vasily <isaev.vs@edu.spbstu.ru>
2018-12-26 12:44:03 +0300

    added realisation of player's methods

--- player.hpp
+++ player.hpp
@@ -12,8 +12,10 @@ class Player
     int yearOfBirthday; // !!update with full data
     int fideRating;
   public:
-    int points;                                // points that player get
-    int additionalOpints;                      // additional points that shoul use when two or more players have same points
+    double points;                                // points that player get
+    double additionalOpints;                      // additional points that shoul use when two or more players have same points
     list<Player> opponents;                    //list of players that thi player played with
     Player(String, String, String, int, int);
+    game (double result, player opponent);
+    printInfo();	
 }
